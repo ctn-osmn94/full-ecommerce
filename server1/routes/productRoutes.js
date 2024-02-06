15b34158cc64988f8aff2cc1f47f48be0c630f6f
osman cetin <96231309+ctn-osmn94@users.noreply.github.com>
2024-02-06 19:56:17 +0300

    Deleted server1 directory

--- server1/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router()
-const productController = require('../controller/productController');
-const categoryController = require('../controller/categoryController')
-
-router.get("/products",productController.getProducts)
-router.get("/categories",categoryController.getProductsByCategory)
-
-module.exports = router
-
--- server1/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const { register,login} = require("../controller/userControllers")
-
-const router = require("express").Router()
-
-router.post("/register",register)
-router.post("/login",login)
-
-module.exports = router
