d90182365b8d37e8d5157eb5a1889c6b017cb365
ctn-osmn94 <cetinosman676@gmail.com>
2024-02-06 20:33:00 +0300

    port düzeltildi

--- server/index.js
+++ server/index.js
@@ -1,28 +1,31 @@
 const express = require("express");
-const cors = require("cors")
-const mongoose = require("mongoose")
-const userRoutes = require("./routes/userRoutes")
-const productRoutes = require("./routes/productRoutes")
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productRoutes");
+const dotenv = require("dotenv");
 
-const app = express()
-require("dotenv").config()
+const app = express();
+dotenv.config();
 
-app.use(cors())
-app.use(express.json())
-app.use("/api/auth", userRoutes)
-app.use("/api", productRoutes)
+const PORT = process.env.PORT || 5000;
 
-const server = app.listen(process.env.PORT, ()=>{
-    console.log(`server started on Port ${process.env.PORT}`);
-})
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", userRoutes);
+app.use("/api", productRoutes);
+
+const server = app.listen(PORT, () => {
+    console.log(`Server started on Port ${PORT}`);
+});
 
 mongoose.connect('mongodb://127.0.0.1:27017/nwusers', {
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
-.then(()=>{
-    console.log("db connection successfull");
-})
-.catch((err)=>{
-    console.log(err.message); 
-})
+    .then(() => {
+        console.log("db connection successfull");
+    })
+    .catch((err) => {
+        console.log(err.message);
+    });
